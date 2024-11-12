3e9b32a9bcb5e8e9b86d14e4af038bb56adc1c9c
Anton Gorshkov <antong@gmail.com>
2024-11-12 18:35:47 -0500

    Try the insane fast model again

--- transcribe.py
+++ transcribe.py
@@ -10,11 +10,11 @@ def transcribe_audio(audio_file):
     with open(audio_file, "rb") as file_input:
         input = { "audio": file_input }
 
-        #model_id = "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c"
-        #model_id = "turian/insanely-fast-whisper-with-video:4f41e90243af171da918f04da3e526b2c247065583ea9b757f2071f573965408" ##slow!!
-        model_id = "openai/whisper:cdd97b257f93cb89dede1c7584e3f3dfc969571b357dbcee08e793740bedd854"
+        model_id = "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c"
+        #model_id = "turian/insanely-fast-whisper-with-video:4f41e90243af171da918f04da3e526b2c247065583ea9b757f2071f573965408" ##slow queing time!!
+        #model_id = "openai/whisper:cdd97b257f93cb89dede1c7584e3f3dfc969571b357dbcee08e793740bedd854"
         output = replicate.run(model_id, input=input)
         # Extract the transcription
         transcription = output.get('transcription', 'No transcription found.')
-        
+
         return transcription
